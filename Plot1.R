5f4b2f8cfd5e0903ddc70ef62a76c532d8bee02b
hemantsriv13 <55556055+hemantsriv13@users.noreply.github.com>
2021-04-03 04:33:58 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,12 +1,12 @@
-file_inp<-read.csv2(file="household_power_consumption.txt",sep=";", header = TRUE)
-start_pt<-"1/2/2007"
-fin_pt<-"2/2/2007"
-req_set1<-file_inp[file_inp$Date==start_pt,]
-req_set2<-file_inp[file_inp$Date==fin_pt,]
-req_set<-rbind(req_set1,req_set2)
-req_set$Global_active_power<-as.numeric(req_set$Global_active_power)
-req_set$Date<-as.Date(req_set$Date, format ="%d/%m/%Y")
-par(mfrow=c(1,1))
-png(file="Plot1.png", height = 480, width = 480)
-hist(as.numeric(req_set$Global_active_power),col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
-dev.off() 
+file_inp<-read.csv2(file="household_power_consumption.txt",sep=";", header = TRUE)  #reading file from local machine
+start_pt<-"1/2/2007"                                                                #starting date to read from
+fin_pt<-"2/2/2007"                                                                  #ending date to read to
+req_set1<-file_inp[file_inp$Date==start_pt,]                                        #extracting 1st of Feb, 2007
+req_set2<-file_inp[file_inp$Date==fin_pt,]                                          #extracting 2nd of Feb, 2007
+req_set<-rbind(req_set1,req_set2)                                                   #row binding them both
+req_set$Global_active_power<-as.numeric(req_set$Global_active_power)                #type conversion to numeric
+req_set$Date<-as.Date(req_set$Date, format ="%d/%m/%Y")                             #changing dates to Date class
+par(mfrow=c(1,1))                                                                   #setting global settings
+png(file="Plot1.png", height = 480, width = 480)                                    #creating png using file device
+hist(as.numeric(req_set$Global_active_power),col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power") #plotting histogram
+dev.off()                                                                           #closing graphic device
